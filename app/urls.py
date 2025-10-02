3782b97f523e64c89c7572de9c450b6ba409ee04
faris-izatou <faris.4haq@gmail.com>
2025-10-02 12:37:07 +0700

    Add Update  for Backend handle in creating Event

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
-from .views import confirm, event_input, dashboard, signup, login_auth, signout
-
-# app_name = 'app'
+from .views import (dashboard_content, events_table,
+                    confirm, dashboard, create_and_manage_events,
+                    signup, login_auth, signout)
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('events/', event_input, name='events'),
+    path('dashboard_content/', dashboard_content, name='dashboard_content'),
+
+    # Events URLs
+    path('events/', dashboard, {'content_type': 'table'}, name='events'),
+    path('events_table/', events_table, name='events_table'),
+
+    # Management URLs
+    path('manage_events/', dashboard,
+         {'content_type': 'manage'}, name='manage_events'),
+    path('manage_events/<uuid:id>/', create_and_manage_events,
+         name='manage_event_detail'),
+    path('create_events/', create_and_manage_events, name='create_events'),
+
+    # Auth URLs
     path('signup/', signup, name='signup'),
     path('login/', login_auth, name='login'),
     path('confirm/', confirm, name='confirm'),
