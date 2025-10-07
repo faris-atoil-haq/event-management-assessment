51840f67176b2ff69bd4c0d50bb40e8f41c41f80
faris-izatou <faris.4haq@gmail.com>
2025-10-07 17:08:41 +0700

    Add test email

--- app/api/views.py
+++ app/api/views.py
@@ -51,6 +51,20 @@ class MyEventsView(generics.ListAPIView):
 
 
 @api_view(['GET'])
+def SendEmailView(request, email=None):
+    if email:
+        res = send_mail('Test Email from Event Management System',
+                        f'This is a test email sent from the https://event-management-system.vercel.app/ with Django backend.',
+                        settings.EMAIL_HOST_USER, [email],
+                        True)
+        if res:
+            return Response({"message": "Email sent successfully."})
+        else:
+            return Response({"message": "Failed to send email."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response({"message": "No email provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_profile(request):
     serializer = UserSerializer(request.user)
